9cb18273babddc90fc9ef3a23e309996b69abb4f
Schluecu <136748981+Schluecu@users.noreply.github.com>
2026-05-09 23:13:33 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openDesc1() {
+  var x = document.getElementById("desc1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
